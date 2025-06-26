a12ad5f5c31e85445f3627398365f14fb83ad511
Jeongil Oh <ohji9876@gmail.com>
2025-06-26 10:31:09 +0900

    Delete opencv_python/06_basic_blur.py

--- opencv_python/06_basic_blur.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-src = cv2.imread("my_input.jpg",cv2.IMREAD_COLOR)
-dst = cv2.blur(src, (21,21), anchor=(-1,1), borderType=cv2.BORDER_DEFAULT)
-
-cv2.imshow("dst", dst)
-cv2.waitKey()
-cv2.destroyAllWindows()
